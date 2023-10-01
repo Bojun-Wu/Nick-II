d1d8b2c943a2f7c920c51d968fa433474f292fa8
Bojun Wu <bojun0817@gmail.com>
2023-10-01 17:38:44 +0800

    configure navbar url

--- history/urls.py
+++ history/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.boy, name = 'history'),
     path('boy/', views.boy, name = 'boy'),
     path('girl/', views.girl, name = 'girl')
 ]
