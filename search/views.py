f8cd8d451c9eba54bbb91fe05fd32a14d39add5b
Bojun Wu <bojun0817@gmail.com>
2023-10-02 01:12:13 +0800

    import jquery and use ajax

--- search/views.py
+++ search/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def search(request):
-    context = {}
-    return render(request, 'search/search.html',context)
+    result = {"name1":"aaa","name2":"aaa","name3":"aaa","name4":"aaa","name5":"aaa"}
+    if request.method=="POST":
+        firstName = request.POST.get('firstName')
+        year = request.POST.get('year')
+        sex = request.POST.get('sex')
+        print("get POST~~~")
+        print(firstName,year,sex)
+        result["name1"] = "bbb"
+        return JsonResponse(result)
+    else:
+        return render(request, 'search/search.html',result)
