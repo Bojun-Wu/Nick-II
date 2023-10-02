764321c4c93e0cb9fc97f2b63fb57246bbe9645e
Bojun Wu <bojun0817@gmail.com>
2023-10-03 07:43:31 +0800

    setup database

--- history/models.py
+++ history/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BabyName(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    name = models.CharField(max_length=200)
+    people = models.PositiveIntegerField()
+    year = models.PositiveSmallIntegerField()
