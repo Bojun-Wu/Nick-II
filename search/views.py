f3f72df7c82ea5b90c21798d825493c9972e9649
Bojun Wu <bojun0817@gmail.com>
2023-10-04 18:23:10 +0800

    form validation

--- search/views.py
+++ search/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from utils.utils import getRecommand
+import time
 
 # Create your views here.
 def search(request):
-    result = {"name1":"aaa","name2":"aaa","name3":"aaa","name4":"aaa","name5":"aaa"}
+    result = {"name1":"John","name2":"Mary","name3":"Robert","name4":"James","name5":"Jennifer"}
     if request.method=="POST":
         firstName = request.POST.get('firstName')
         year = request.POST.get('year')
         sex = request.POST.get('sex')
         sex = "male" if sex=="男" else "female"
 
-        print("get POST~~~")
-        print(firstName,year,sex)
+        print(firstName, year, sex)
+        print(type(firstName),type(year),type(sex))
 
-        recommand = getRecommand(year,sex,firstName)
-        seq = 0
-        for i in result:
-            result[i] = recommand[seq]
-            seq+=1
+        result = {"name1":"Hello","name2":"Got","name3":"One","name4":"Request","name5":"Jennifer"}
+        time.sleep(2)
+
+        # recommand = getRecommand(year,sex,firstName)
+        # seq = 0
+        # for i in result:
+        #     result[i] = recommand[seq]
+        #     seq+=1
 
         return JsonResponse(result)
     else:
