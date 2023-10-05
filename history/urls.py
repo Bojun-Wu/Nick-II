6a4ffb356549e1bba27086917b0aaec042ecceb0
Bojun Wu <bojun0817@gmail.com>
2023-10-05 22:47:41 +0800

    ready to deploy

--- history/urls.py
+++ history/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:gender>/',views.history, name='history')
-    # path('', views.boy, name = 'history'),
-    # path('boy/', views.boy, name = 'boy'),
-    # path('girl/', views.girl, name = 'girl')
+    path('<str:gender>/',views.history, name='history'),
 ]
--- name_helper/settings.py
+++ name_helper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ev$j*@c6m4tv-a1%3u_r5!f2&&pxn#$bpazl@6@3pt)dq*m8&w'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,3 +130,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# # https setting
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+# HSTS setting
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
