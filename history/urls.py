fb3abb11657efae6acef2e1faea215841055f563
Bojun Wu <bojun0817@gmail.com>
2023-10-05 15:47:51 +0800

    combine boy and girl page

--- history/urls.py
+++ history/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.boy, name = 'history'),
-    path('boy/', views.boy, name = 'boy'),
-    path('girl/', views.girl, name = 'girl')
+    path('<str:gender>/',views.history, name='history')
+    # path('', views.boy, name = 'history'),
+    # path('boy/', views.boy, name = 'boy'),
+    # path('girl/', views.girl, name = 'girl')
 ]
--- history/views.py
+++ history/views.py
@@ -4,47 +4,16 @@ from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 
 # Create your views here.
-def boy(request):
-    data = BabyName.objects.filter(year = 2010).filter(gender="M").order_by('rank')
-    context = {'data':data}
+def history(request, gender):
+    sex = "M" if gender=="boy" else "F"
+    data = BabyName.objects.filter(year = 2010).filter(gender=sex).order_by('rank')
+    context = {'gender':gender, 'data':data}
+
     if request.method=="POST":
         year = int(request.POST.get('decade'))
-        data = BabyName.objects.filter(year = year).filter(gender="M").order_by('rank')
+        data = BabyName.objects.filter(year = year).filter(gender=sex).order_by('rank')
         data_json = serializers.serialize('json',data)
         return HttpResponse(data_json,content_type='application/json')
 
     else:
-        return render(request,'history/boypage.html',context)
-
-def girl(request):
-    data = BabyName.objects.filter(year = 2010).filter(gender="F").order_by('rank')
-    context = {'data':data}
-    if request.method=="POST":
-        year = int(request.POST.get('decade'))
-        data = BabyName.objects.filter(year = year).filter(gender="F").order_by('rank')
-        data_json = serializers.serialize('json',data)
-        return HttpResponse(data_json,content_type='application/json')
-
-    else:
-        return render(request,'history/girlpage.html',context)
-
-# def girl(request):
-#     data = BabyName.objects.filter(year = 2010).filter(gender="F").order_by('rank')
-#     context = {'data':data}
-#     if request.method=="POST":
-#         year = int(request.POST.get('decade'))
-#         # print("got POST",year)
-#         data = BabyName.objects.filter(year = year).filter(gender="F").order_by('rank')
-#         # data = {1:{'name':'Matthew','year':1990,'number':'351,649'}}
-#         # print(type(data))
-#         data = list(data)
-#         output = {}
-#         print(len(data))
-#         for i in range(len(data)):
-#             output[i] = {}
-#             output[i]['name']=data[i].name
-#             output[i]['people']=data[i].people
-#         return JsonResponse(output)
-
-#     else:
-#         return render(request,'history/girlpage.html',context)
+        return render(request,'history/history.html',context)
