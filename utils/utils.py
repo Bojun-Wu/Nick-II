9f0766469b046272cb409f3e2f2dbfb5a90f77bb
Bojun Wu <bojun0817@gmail.com>
2023-10-05 20:55:20 +0800

    add requirement file

--- utils/utils.py
+++ utils/utils.py
@@ -10,46 +10,12 @@ def transName(name):
 
 
 # g4f free openai api
-def getRecommand(year, sex, name, model="gpt-3.5-turbo"):
-
-    nameSap = transName(name)
-
-    response = g4f.ChatCompletion.create(
-        stream=False,
-        model=model,
-        messages=[
-            {
-                "role": "user", 
-                "content": "接下來我會給你一個嬰兒的出生年份、它中文姓名的讀音、它的性別，請建議它五個適合的英文名字，這些名字需要與它的名字的中文發音相似，適合它的性別，並且符合它的出生年代，請只輸出5個英文名字，不需要額外其他資訊',"
-            },
-            {
-                "role": "user",
-                "content": "2000, male, bo jun"
-            },
-            {
-                "role": "assistant",
-                "content": 'Bowen, Bob, Bryan, Johan, Jon'
-            },
-            {
-                "role": "user",
-                "content": f"{year}, {sex}, {nameSap}"
-            },
-        ],
-    )
-    print(response)
-    returnContent = list(response.split(", "))
-
-    return returnContent
-
-
-# def getRecommand(year, sex, name, model="gpt-3.5-turbo"):
+# gpt-3.5-turbo, gpt-4
+# def getRecommand(year, sex, name, model="gpt-4"):
 
 #     nameSap = transName(name)
 
-#     openai.api_key = config('OPENAI_API_KEY')
-#     openai.api_base = config('OPENAI_API_BASE')
-
-#     chat_completion = openai.ChatCompletion.create(
+#     response = g4f.ChatCompletion.create(
 #         stream=False,
 #         model=model,
 #         messages=[
@@ -71,8 +37,43 @@ def getRecommand(year, sex, name, model="gpt-3.5-turbo"):
 #             },
 #         ],
 #     )
-
-#     returnContent = chat_completion['choices'][0]['message']['content']
-#     returnContent = list(returnContent.split(", "))
+#     print(response)
+#     returnContent = list(response.split(", "))
 
 #     return returnContent
+
+
+def getRecommand(year, sex, name, model="gpt-3.5-turbo"):
+
+    nameSap = transName(name)
+
+    openai.api_key = config('OPENAI_API_KEY')
+    openai.api_base = config('OPENAI_API_BASE')
+
+    chat_completion = openai.ChatCompletion.create(
+        stream=False,
+        model=model,
+        messages=[
+            {
+                "role": "user", 
+                "content": "接下來我會給你一個嬰兒的出生年份、它中文姓名的讀音、它的性別，請建議它五個適合的英文名字，這些名字需要與它的名字的中文發音相似，適合它的性別，並且符合它的出生年代，請只輸出5個英文名字，不需要額外其他資訊',"
+            },
+            {
+                "role": "user",
+                "content": "2000, male, bo jun"
+            },
+            {
+                "role": "assistant",
+                "content": 'Bowen, Bob, Bryan, Johan, Jon'
+            },
+            {
+                "role": "user",
+                "content": f"{year}, {sex}, {nameSap}"
+            },
+        ],
+    )
+
+    returnContent = chat_completion['choices'][0]['message']['content']
+    returnContent = list(returnContent.split(", "))
+
+    return returnContent
