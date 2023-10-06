603392a7c2aa9a23e74c4cc1999cc482ec25f7ca
Bojun Wu <bojun0817@gmail.com>
2023-10-06 19:29:26 +0800

    try deploy

--- name_helper/settings.py
+++ name_helper/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'whitenoise.runserver_nostatic',
     'search.apps.SearchConfig',
     'history.apps.HistoryConfig',
 ]
@@ -125,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles/")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
